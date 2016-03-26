7501c388cae1ed67fcec782d2f2409004bcf4c52
mgk03 <mgk03@AndroidBoi.fios-router.home>
2016-03-26 12:39:32 -0400

    Finished Server/Hardware page. Began work on the code examples pages.

--- public_html/js/MattNavBar/NavBarRandomColor.js
+++ public_html/js/MattNavBar/NavBarRandomColor.js
@@ -76,3 +76,33 @@ function ran_col() { //function name
     color += letters[Math.floor(Math.random() * letters.length)];
     document.getElementById('PlaceholderG').style.background = color; // Setting the random color on your div element.
 }
+
+//LCARS random color setter
+//Runs on page load and picks a random LCARS common color for the background of an element
+function ran_colb() { //function name
+    var letters = ['FF9900','CC99CC','9999CC','CC6666','FFCC99','9999FF','FF9966','CC6699']; //Set your colors here
+    
+    //See lines 7+8 and 12 - 14 for detailed information on this code segment
+    //For ID PlaceholderC
+    var color='#';
+    color += letters[Math.floor(Math.random() * letters.length)];
+    document.getElementById('CPP').style.background = color; // Setting the random color on your div element.
+    
+    //See lines 7+8 and 12 - 14 for detailed information on this code segment
+    //For ID PlaceholderC
+    var color='#';
+    color += letters[Math.floor(Math.random() * letters.length)];
+    document.getElementById('Java').style.background = color; // Setting the random color on your div element.
+    
+    //See lines 7+8 and 12 - 14 for detailed information on this code segment
+    //For ID PlaceholderC
+    var color='#';
+    color += letters[Math.floor(Math.random() * letters.length)];
+    document.getElementById('html').style.background = color; // Setting the random color on your div element.
+    
+    //See lines 7+8 and 12 - 14 for detailed information on this code segment
+    //For ID PlaceholderC
+    var color='#';
+    color += letters[Math.floor(Math.random() * letters.length)];
+    document.getElementById('php').style.background = color; // Setting the random color on your div element
+}
